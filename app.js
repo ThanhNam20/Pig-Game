e194a0dfb34fc5e9aa5368b061fbbd2a540591b4
ThanhNam20 <khuatthanhnam@gmail.com>
2020-01-07 22:07:34 +0700

    Updating Score and Changing Active Player

--- app.js
+++ app.js
@@ -20,5 +20,26 @@ document.querySelector('.btn-roll').addEventListener('click', () => {
     diceDOM.style.display = 'block';
     diceDOM.src = "dice-" + dice + '.png';
     //update the score if its not 1
+    if (dice != 1) {
+        //adding score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //next player
+
+        // if (activePlayer === 0) {
+        //     activePlayer = 1;
+        // } else {
+        //     activePlayer = 0;
+        // }       
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        // cau truc rut gon cua if else
+        roundScore = 0;
+        document.querySelector('#current-0').textContent = '0';
+        document.querySelector('#current-1').textContent = '0';
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none'; 
+    }
 })
 
